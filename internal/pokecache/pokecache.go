1b9eb0f8bd1709d48bf19fb4931adadf1049e3d1
Joenatan <Joenatan506@gmail.com>
2026-01-15 17:58:11 +0700

    Added pokecache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,35 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	mu       sync.Mutex
+	entries  map[string]cacheEntry
+	interval time.Duration
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) *Cache {
+	cache := Cache{
+		entries:  map[string]cacheEntry{},
+		interval: interval,
+	}
+	return &cache
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	cacheentry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.entries[key] = cacheentry
+}
